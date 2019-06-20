2dd446d7ce455ae0f7076772b4dc1de37d6c9e1a
katupitiya <47234548+katupitiya@users.noreply.github.com>
2019-06-20 06:13:56 +0530

    Update App.js

--- student_information_system_frontend/src/App.js
+++ student_information_system_frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
         </a>
       </header>
     </div>
